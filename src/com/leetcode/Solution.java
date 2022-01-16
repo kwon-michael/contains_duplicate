69d84b74c3611f8de45d4809d2b7888248f49ba5
kwon-michael <90662213+kwon-michael@users.noreply.github.com>
2022-01-15 21:21:42 -0500

    pushed

--- src/com/leetcode/Solution.java
+++ src/com/leetcode/Solution.java
@@ -1,6 +1,7 @@
 package com.leetcode;
 
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class Solution {
@@ -22,6 +23,20 @@ public class Solution {
             else{
                 set.add(i);
 
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i =0; i < nums.length; i++){
+            int current = nums[i];
+            if (map.containsKey(current) && i- map.get(current) <= k ){
+                return true;
+            }
+            else{
+                map.put(current,i);
             }
         }
         return false;
