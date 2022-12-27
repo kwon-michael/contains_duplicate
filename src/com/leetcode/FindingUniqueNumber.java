11ea994e184620ba1134c93b5458dbd1ea060149
Chil's Coding World <chilhoank@yahoo.ca>
2022-12-27 13:09:37 -0500

    Apply TreeSet to FindinguniqNumber
    Applied TreeSet DataStructure to the FindingUniqueNumber solution.

--- src/com/leetcode/FindingUniqueNumber.java
+++ src/com/leetcode/FindingUniqueNumber.java
@@ -2,10 +2,12 @@ package com.leetcode;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class FindingUniqueNumber {
     public static void main(String[] args) {
         HashSet<Integer> uniqueSet = new HashSet<>();
+        TreeSet<Integer> treeSet = new TreeSet<>();
         int[] testArray = {1,1,2,2,3,3,3,49,90};
         for(int i: testArray){
             if (uniqueSet.contains(i)){
@@ -15,10 +17,19 @@ public class FindingUniqueNumber {
             }
 
         }
+        for(int i: testArray) {
+        	if(treeSet.contains(i)) {
+        		continue;
+        	}else {
+        		treeSet.add(i);
+        	}
+        }
         uniqueSet.stream().forEach(System.out::println);
 //        for(int i: uniqueSet){
 //            System.out.println(i);
 //        }
+        System.out.println("sorted order");
+        treeSet.stream().forEach(System.out::println);
 
 
     }
